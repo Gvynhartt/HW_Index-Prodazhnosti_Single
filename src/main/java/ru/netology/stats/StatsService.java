456264e89cae935674f7e7699fe7858d6cbee6b6
Gvynhartt <savshush134@gmail.com>
2023-02-11 21:01:37 +0300

    repeating logics pruned for certain methods

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -4,98 +4,81 @@ public class StatsService {
 
     public int calcSalesTotal(int[] salesByMonths) { // подсчитывает общие продажи
 
-       /* int[] salesByMonths = {
-                8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18
-        };*/
-        int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
+        // int arrayPosition = 0; ух ты, мы сэкономили аж ОДНУ строчку кода
         int currentTotal = 0; // показывает текущий результат сложения продаж по месяцам
 
-        for (int iterSalesByMonths : salesByMonths) {
-            if (arrayPosition <= salesByMonths.length) {
+
+            for (int arrayPosition = 0; arrayPosition < salesByMonths.length; arrayPosition++) {
                 currentTotal = (currentTotal + salesByMonths[arrayPosition]);
-                arrayPosition = arrayPosition + 1;
-                // System.out.println("Перебираемое значение: " + currentTotal); данная строчка добавлена для контроля выполнения
+                // arrayPosition = arrayPosition + 1; даже ДЖВЕ строчки сэкономили... а в чём смысл? for-each делает то же
+                // самое, с менее громоздкой записью, ещё и не выдаёт исключений при достижении конца массива
             }
-        }
         return currentTotal;
     }
 
     public int calcMonthlySales(int[] salesByMonths) { // подсчитывает средние продажи в месяц
+        // ДОРАБОТАН С ЗАМЕНОЙ КОПИПАСТНОЙ ЛОГИКИ ВЫЗОВОМ МЕТОДА
 
-        int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
-        int currentTotal = 0; // показывает текущий результат сложения продаж по месяцам
-
-        for (int iterSalesByMonths : salesByMonths) {
+        /*for (int iterSalesByMonths : salesByMonths) {
             if (arrayPosition <= salesByMonths.length) {
                 currentTotal = (currentTotal + salesByMonths[arrayPosition]);
                 arrayPosition = arrayPosition + 1;
                 // System.out.println("Перебираемое значение: " + currentTotal); данная строчка добавлена для контроля выполнения
             }
-        }
-        return (currentTotal / salesByMonths.length); // а почему я должен делать этот пункт НЕ копипастом?
+        }*/
+        return (calcSalesTotal(salesByMonths) / salesByMonths.length); // вычисление средних продаж сокращено до этой строки
+        // с использованием вызова метода подсчёта общих продаж
     }
 
     public int calcMaxSalesMonthNmb(int[] salesByMonths) { // находит месяц максимальных продаж
 
-        int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
         int currentMax = salesByMonths[0]; // сохраняет максимальное значение из всех перебранных на текущей итерации
         int maxSalesMonth = 0; // сохраняет номер месяца с максимальными продажами
 
-        for (int iterSalesByMonths : salesByMonths) {
-            if (arrayPosition <= salesByMonths.length) {
-                if (currentMax <= salesByMonths[arrayPosition]) {
-                    currentMax = salesByMonths[arrayPosition];
-                    maxSalesMonth = arrayPosition + 1;
-                    arrayPosition = arrayPosition + 1;
-                } else {
-                    arrayPosition = arrayPosition + 1;
-                }
-                // System.out.println("Перебираемое значение: " + salesByMonths[arrayPosition - 1] + ", максимальные продажи: " + maxSalesMonth);
-                // данная строчка добавлена для контроля выполнения
+        for (int arrayPosition = 0; arrayPosition < salesByMonths.length; arrayPosition++) { // ПЕРЕПИСАНО С FOR
+            if (currentMax <= salesByMonths[arrayPosition]) {
+                currentMax = salesByMonths[arrayPosition];
+                maxSalesMonth = arrayPosition + 1;
             }
         }
         return maxSalesMonth;
     }
 
-    public int calcMinSalesMonthNmb(int[] salesByMonths) { // находит месяц минимальных продаж
+        public int calcMinSalesMonthNmb(int[] salesByMonths) { // находит месяц минимальных продаж
 
-        int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
         int currentMin = salesByMonths[0]; // сохраняет минимальное значение из всех перебранных на текущей итерации
         int minSalesMonth = 0; // сохраняет номер месяца с минимальными продажами
 
-        for (int iterSalesByMonths : salesByMonths) {
-            if (arrayPosition <= salesByMonths.length) {
+        for (int arrayPosition = 0; arrayPosition < salesByMonths.length; arrayPosition++) { // ПЕРЕПИСАНО С FOR
                 if (currentMin >= salesByMonths[arrayPosition]) {
                     currentMin = salesByMonths[arrayPosition];
                     minSalesMonth = arrayPosition + 1;
-                    arrayPosition = arrayPosition + 1;
-                } else {
-                    arrayPosition = arrayPosition + 1;
                 }
-                // System.out.println("Перебираемое значение: " + salesByMonths[arrayPosition - 1] + ", минимальные продажи: " + minSalesMonth);
-                // данная строчка добавлена для контроля выполнения
             }
-        }
-        return minSalesMonth; // копипаст по-прежнему сила
+        return minSalesMonth;
     }
 
-    public int calcMonthsBelowMean(int[] salesByMonths) {  // подсчитывает число месяцев с продажами ниже среднего
+    public int calcMonthsBelowMean(int[] salesByMonths) {  // подсчитывает число месяцев с продажами ниже среднего;
+        // ДОРАБОТАН С ЗАМЕНОЙ КОПИПАСТНОЙ ЛОГИКИ ВЫЗОВОМ МЕТОДА
 
-        int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
-        int currentTotal = 0; // показывает текущий результат сложения продаж по месяцам
+        // int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
+        // int currentTotal = 0; // показывает текущий результат сложения продаж по месяцам
 
-        for (int iterSalesByMonths : salesByMonths) {
+        /*for (int iterSalesByMonths : salesByMonths) {
             if (arrayPosition <= salesByMonths.length) {
                 currentTotal = (currentTotal + salesByMonths[arrayPosition]);
                 arrayPosition = arrayPosition + 1;
                 // System.out.println("Перебираемое значение: " + currentTotal); данная строчка добавлена для контроля выполнения
             }
-        }
-        arrayPosition = 0;
-        int meanSalesHic = (currentTotal / salesByMonths.length); // на всякий случай назовём переменную по-другому во избежание путаницы
+        }*/
+
+        int arrayPosition = 0;
+        int meanSalesHic = calcMonthlySales(salesByMonths); // подсчёт среднего сокращён до вызова метода, вычисляющего
+        // среднемесячные продажи
         int monthsBelowMean = 0; // сохраняет число месяцев с продажами ниже среднего
 
-        for (int iterSalesByMonths : salesByMonths) {
+        for (int iterSalesByMonths : salesByMonths) { // конкретно здесь итеративная переменная таки используется, поэтому for-each оправдан
+            // да, в for тоже можно вызвать перебираемое значение, но в таком виде это проще, ИМХО
             if (arrayPosition <= salesByMonths.length) {
                 if (iterSalesByMonths < meanSalesHic) {
                     monthsBelowMean = monthsBelowMean + 1;
@@ -110,26 +93,28 @@ public class StatsService {
     }
 
     public int calcMonthsAboveMean(int[] salesByMonths) { // подсчитывает число месяцев с продажами выше среднего
+        // ДОРАБОТАН С ЗАМЕНОЙ КОПИПАСТНОЙ ЛОГИКИ ВЫЗОВОМ МЕТОДА
 
-        int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
-        int currentTotal = 0; // показывает текущий результат сложения продаж по месяцам
+        // int arrayPosition = 0; // показывает номер перебираемой ячейки в массиве
+        // int currentTotal = 0; // показывает текущий результат сложения продаж по месяцам
 
-        for (int iterSalesByMonths : salesByMonths) {
+        /*for (int iterSalesByMonths : salesByMonths) {
             if (arrayPosition <= salesByMonths.length) {
                 currentTotal = (currentTotal + salesByMonths[arrayPosition]);
                 arrayPosition = arrayPosition + 1;
                 // System.out.println("Перебираемое значение: " + currentTotal); данная строчка добавлена для контроля выполнения
             }
-        }
-        arrayPosition = 0;
-        int meanSalesHic = (currentTotal / salesByMonths.length); // на всякий случай назовём переменную по-другому во избежание путаницы
+        }*/
+
+        int arrayPosition = 0;
+        int meanSalesHic = calcMonthlySales(salesByMonths); // подсчёт среднего сокращён до вызова метода, вычисляющего
+        // среднемесячные продажи
         int monthsAboveMean = 0; // сохраняет число месяцев с продажами выше среднего
 
-        for (int iterSalesByMonths : salesByMonths) {
+        for (int iterSalesByMonths : salesByMonths) { // конкретно здесь итеративная переменная таки используется, поэтому for-each оправдан
             if (arrayPosition <= salesByMonths.length) {
                 if (iterSalesByMonths > meanSalesHic) {
                     monthsAboveMean = monthsAboveMean + 1;
-                    // System.out.println("Всего месяцев с продажами выше среднего: " + monthsAboveMean);
                     arrayPosition = arrayPosition + 1;
                 } else {
                     arrayPosition = arrayPosition + 1;
